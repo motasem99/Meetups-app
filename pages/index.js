318e9213a777660f2bf7a3efa404ef16a27a51e3
mutasem <motasemimad10@gmail.com>
2021-08-31 17:13:58 +0300

    rendering A list of (dummy) meetups

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg',
+    address: 'Palestine, Gaza',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg',
+    address: 'Palestine, Gaza',
+    description: 'This is a second meetup!',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
