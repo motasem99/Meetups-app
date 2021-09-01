f9e55388d6629b68cf2f96a0e6f8e6c112699c16
mutasem <motasemimad10@gmail.com>
2021-09-01 13:52:23 +0300

    adding custom components & styling with css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg'
+      title='A first meetup'
+      address='Palestine, Gaza'
+      description='The meetup description'
+    />
+  );
+}
+
+export default MeetupDetails;
