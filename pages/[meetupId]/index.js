aab5fc23441b0172043203d0f3cc3749ea2d3c61
mutasem <motasemimad10@gmail.com>
2021-09-01 14:55:23 +0300

    working with dynamic paths params in getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,8 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
+  const { meetupData } = props;
+
   return (
     <MeetupDetail
       image='https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg'
@@ -9,6 +11,23 @@ function MeetupDetails() {
       description='The meetup description'
     />
   );
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg',
+        id: meetupId,
+        title: 'A first meetup',
+        address: 'Palestine, Gaza',
+        description: 'The meetup description',
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
