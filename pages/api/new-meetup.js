eece0545b69cf2cdfb6f53825e5581c5e7ff4c4f
mutasem <motasemimad10@gmail.com>
2021-09-01 17:49:15 +0300

    connecting & quering a mongodb database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,22 @@
-function handler(req, res) {
+const { MongoClient } = require('mongodb');
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://mutasem-NextjsProject:2741999@cluster0.6sm4t.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
